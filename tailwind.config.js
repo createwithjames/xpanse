f1e4959185beda89b45d016822eef35417fce243
james-phillips-haysto <101575361+james-phillips-haysto@users.noreply.github.com>
2023-11-21 22:17:25 +0000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.antlers.html",
+        "./resources/**/*.antlers.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.vue",
+        "./content/**/*.md",
+    ],
+
+    theme: {
+        fontFamily: {
+            sans: ["Inter", "sans-serif"],
+            display: ["Lexend", "sans-serif"],
+        },
+        extend: {},
+    },
+
+    plugins: [require("@tailwindcss/typography")],
+};
